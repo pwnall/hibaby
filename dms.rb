2a6e3e2ff77d90c068da973c8fe5a982cd43fb95
Victor Costan <costan@gmail.com>
2014-03-20 19:54:26 -0400

    Mostly working DMS scraper.

--- dms.rb
+++ dms.rb
@@ -1,3 +1,7 @@
+require 'net/https'
+require 'set'
+require 'uri'
+
 require 'webkit_remote'
 
 # Inputs text in an input box.
@@ -72,6 +76,8 @@ end
 
 # True if a DOM element is on the page, false if it is outside the page.
 #
+# @param {WebkitRemote::Client} client
+# @param {WebKitRemote::Client::DomNode} dom_node
 # @return {Boolean} true if an element is positioned within the page's
 #     boundaries, and false if it is off; this is usually the case for non-UI
 #     elements
@@ -86,6 +92,8 @@ end
 
 # The text inside a DOM element.
 #
+# @param {WebkitRemote::Client} client
+# @param {WebKitRemote::Client::DomNode} dom_node
 # @return {String} the text inside the element; this is the value of the
 #     textContent property (also known as innerText)
 def element_text_content(client, dom_node)
@@ -95,6 +103,21 @@ def element_text_content(client, dom_node)
   }
 END_JS
   client.remote_eval('window').bound_call js_code, dom_node.js_object
+  # NOTE: this returns a String, we don't need to release it
+end
+
+# Scrolls the browser view in such a way that a DOM element is in the viewport.
+#
+# @param {WebkitRemote::Client} client
+# @param {WebKitRemote::Client::DomNode} dom_node
+def scroll_to(client, dom_node)
+  js_code = <<END_JS
+  function (domNode) {
+    domNode.scrollIntoView();
+  }
+END_JS
+  result = client.remote_eval('window').bound_call js_code, dom_node.js_object
+  result.release
 end
 
 # Logs into the DMS site.
@@ -134,12 +157,12 @@ end
 
 # Makes a search on the DMS site.
 #
-# This assumes the user is not logged into the site.
+# This assumes a user is logged into the site.
 #
 # @param {WebkitRemote::Client} client
-# @param {String} email
-# @param {String} password
-def dms_search(client, email, password)
+# @param {Hash<String, String>} criteria the search criteria, e.g.
+#     "Schools": "MIT"
+def dms_issue_search(client, criteria)
   client.page_events = true
   client.navigate_to 'https://datemyschool.com/'
   client.wait_for type: WebkitRemote::Event::PageLoaded
@@ -169,38 +192,161 @@ def dms_search(client, email, password)
     dropdown_containers[element_text_content(client, label)] = container
   end
 
-  category = 'Schools'
-  category_choice = 'MIT'
-
-  # Semi-generic code for working a "fancy" dropdown.
-  dropdown = dropdown_containers[category]
-  click_element client, dropdown.query_selector('.arrow')
-  dropdown_select = dropdown.query_selector('select')
-  dropdown_options = {}
-  dropdown_select.query_selector_all('option').each do |option|
-    text = element_text_content(client, option)
-    dropdown_options[text] = option.attributes['value'].strip
+  criteria.each do |category, category_choice|
+    # Semi-generic code for working a "fancy" dropdown.
+    dropdown = dropdown_containers[category]
+    click_element client, dropdown.query_selector('.arrow')
+    dropdown_select = dropdown.query_selector('select')
+    dropdown_options = {}
+    dropdown_select.query_selector_all('option').each do |option|
+      text = element_text_content(client, option)
+      dropdown_options[text] = option.attributes['value'].strip
+    end
+    # The "fancy" dropdown options aren't in the container's tree.
+    option_id = dropdown_options[category_choice]
+    unless option_id
+      raise "Failed to find option #{category_choice} for category #{category}."
+    end
+    dropdown_li = client.dom_root.
+        query_selector_all("li[data-id=#{option_id.inspect}]").find do |li|
+      element_text_content(client, li).strip == category_choice
+    end
+    if dropdown_li.nil?
+      raise "Failed to find <li> for option #{category_choice} in #{category}."
+    end
+    click_element client, dropdown_li
   end
-  # The "fancy" dropdown options aren't in the container's tree.
-  option_id = dropdown_options[category_choice]
-  unless option_id
-    raise "Failed to find option #{category_choice} for category #{category}."
-  end
-  dropdown_li = client.dom_root.
-      query_selector_all("li[data-id=#{option_id.inspect}]").find do |li|
-    element_text_content(client, li).strip == category_choice
-  end
-  if dropdown_li.nil?
-    raise "Failed to find <li> for option #{category_choice} in #{category}."
-  end
-  click_element client, dropdown_li
 
   search_button = client.dom_root.query_selector(
       'form[action*="search"] input[type="submit"]')
   click_element client, search_button
+  client.page_events = false
+  client.clear_all
 end
 
+# Scrolls the DMS search screen to the bottom, so it loades more matches.
+#
+# @param {WebkitRemote::Client} client
+# @return {Boolean} true if scrolling to the bottom returned more matches,
+#     false otherwise
+def dms_search_scroll(client)
+  last_page_div = client.dom_root!.
+      query_selector_all('.browse[data-page-num]').last
+  last_page_num = last_page_div.attributes['data-page-num'].strip
+
+  scroll_to client, last_page_div
+  client.clear_all
+
+  # HACK(pwnall): should use a DOM breakpoint or some network listener instead
+  #     of this hacked up approach
+  300.times do
+    sleep 2
+    new_last_page_div = client.dom_root.query_selector_all(
+        '.browse[data-page-num]').last
+    new_last_page_num = new_last_page_div.attributes['data-page-num'].strip
+    return true if new_last_page_num != last_page_num
+  end
+  false
+end
+
+# Extracts results from a DMS page div.
+#
+# @param {WebkitRemote::Client} client
+# @param {WebKitRemote::Client::DomNode} page_div a DOM node containing a
+#     "page" of search results
+def dms_get_results(client, page_div)
+  results = []
+  page_div.query_selector_all('div[data-user-id]').each do |result_div|
+    profile_id = result_div.attributes['data-user-id'].strip
+    profile_link = page_div.query_selector(
+        "a[href*=\"profile\"][href*=#{profile_id.inspect}")
+    if profile_link
+      profile_url = profile_link.attributes['href']
+    else
+      profile_url = nil
+    end
+
+    picture_img = result_div.query_selector(
+        "img[src*=pics][src*=#{profile_id.inspect}]")
+    if picture_img
+      picture_url = picture_img.attributes['src']
+    else
+      picture_url = nil
+    end
+
+    details = {}
+    [
+      [:username, '.nickname .username'],
+      [:age, '.basics .age'],
+      [:uni, '.uni'],
+      [:school, '.school'],
+    ].each do |detail|
+      key = detail[0]
+      selector = detail[1]
+      detail_element = result_div.query_selector(".profile-info #{selector}")
+      if detail_element
+        details[key] = element_text_content client, detail_element
+      end
+    end
+
+    if picture_img
+      results << {
+        id: profile_id,
+        url: profile_url,
+        pic: picture_url,
+        details: details,
+      }
+    end
+  end
+  results
+end
+
+# Makes a search on the DMS site.
+#
+# This assumes a user is logged into the site.
+#
+# @param {WebkitRemote::Client} client
+# @param {Hash<String, String>} criteria the search criteria, e.g.
+#     "Schools": "MIT"
+# @yield
+def dms_search(client, criteria)
+  dms_issue_search client, criteria
+
+  seen_pages = Set.new
+
+  done_scrolling = false
+  loop do
+    page_divs = client.dom_root.query_selector_all('.browse[data-page-num]')
+    3.times do
+      begin
+        page_divs.each do |page_div|
+          page_num = page_div.attributes['data-page-num'].strip
+          next if seen_pages.include?(page_num)
+
+          page_results = dms_get_results client, page_div
+          page_results.each do |page_result|
+            yield page_result
+          end
+
+          seen_pages << page_num
+        end
+        break
+      rescue RuntimeError
+        # The DOM got modified as we were scanning the page.
+      end
+    end
+
+    break if done_scrolling
+    done_scrolling = true unless dms_search_scroll(client)
+  end
+end
 
 # NOTE: allow_popups is necessary to be able to click on the "search" link
 client = WebkitRemote.local window: { width: 1024, height: 768 }
 dms_login client, 'leemoh@mit.edu', 'moresparkles'
+dms_search client, 'Schools' => ARGV[0] do |result|
+  File.write "#{result[:id]}.json", JSON.dump(result)
+  pic_result = Net::HTTP.get URI.parse(result[:pic])
+  File.binwrite "#{result[:id]}.jpg", pic_result
+end
+client.close_browser
